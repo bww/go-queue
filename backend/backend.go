5bee6e76bd05a64cbe78ef2264e4cd8e57a1e336
Brian W. Wolter <brian@brianwolter.com>
2021-06-07 21:19:25 -0400

    Pass through options

--- backend/backend.go
+++ backend/backend.go
@@ -8,16 +8,17 @@ import (
 	"github.com/bww/go-queue"
 	"github.com/bww/go-queue/backend/blackhole"
 	"github.com/bww/go-queue/backend/pubsub"
+	"github.com/bww/go-queue/config"
 )
 
-func New(dsn string) (queue.Queue, error) {
+func New(dsn string, opts ...config.Option) (queue.Queue, error) {
 	u, err := url.Parse(dsn)
 	if err != nil {
 		return nil, err
 	}
 	switch strings.ToLower(u.Scheme) {
 	case pubsub.Scheme:
-		return pubsub.New(dsn)
+		return pubsub.New(dsn, opts...)
 	case blackhole.Scheme:
 		return blackhole.New(dsn)
 	default:
